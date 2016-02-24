c733eecdb7e69eb4b21992ef8c35cef1852c4a09
KevinBozic <bozic.kevin@gmail.com>
2016-02-24 12:00:41 -0500

    Initail import

---
+++ src/com/theironyard/Message.java
@@ -0,0 +1,15 @@
+package com.theironyard;//Created by KevinBozic on 2/24/16.
+
+public class Message {
+    int id;
+    int replyId;
+    String author;
+    String text;
+
+    public Message(int id, int replyId, String author, String text) {
+        this.id = id;
+        this.replyId = replyId;
+        this.author = author;
+        this.text = text;
+    }
+}
---
+++ src/com/theironyard/User.java
@@ -0,0 +1,11 @@
+package com.theironyard;//Created by KevinBozic on 2/24/16.
+
+public class User {
+    String name;
+    String password;
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
